ffb44978f923fde592e1c67510e238f1971fc687
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-09 20:25:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,12 +14,12 @@
 
 
 
-def speak_to_grandma(string)
-  if string == string.upcase
-  print "NO, NOT SINCE 1938"
-  elsif string == "I LOVE YOU GRANDMA!"
-  print "I LOVE YOU TOO PUMPKIN!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
   else
-  print "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
